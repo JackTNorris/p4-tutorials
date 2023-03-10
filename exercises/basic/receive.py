fed5cff3cc26853c550279e5613df6f4c4e2e4a5
Jack Norris <jacktimothynorris@gmail.com>
2023-03-10 19:07:05 +0000

    added p4 shit

--- exercises/basic/receive.py
+++ exercises/basic/receive.py
@@ -1,60 +1,68 @@
-#!/usr/bin/env python3
-import os
+import socket
+import struct
+import math
 import sys
 
-from scapy.all import (
-    TCP,
-    UDP,
-    FieldLenField,
-    FieldListField,
-    IntField,
-    IPOption,
-    ShortField,
-    get_if_list,
-    sniff
-)
-from scapy.layers.inet import _IPOption_HDR
+sys.path.append('../')
 
+from jpt_algo_evaluation.jpt_algo import calculate_complex_voltage, jpt_algo, phase_angle_and_magnitude_from_complex_voltage, calculate_approximation_error
 
-def get_if():
-    ifs=get_if_list()
-    iface=None
-    for i in get_if_list():
-        if "eth0" in i:
-            iface=i
-            break;
-    if not iface:
-        print("Cannot find eth0 interface")
-        exit(1)
-    return iface
+UDP_IP_ADDRESS = "0.0.0.0"  # listen on all available interfaces
+UDP_PORT_NO = 4712  # PMU data port number
 
-class IPOption_MRI(IPOption):
-    name = "MRI"
-    option = 31
-    fields_desc = [ _IPOption_HDR,
-                    FieldLenField("length", None, fmt="B",
-                                  length_of="swids",
-                                  adjust=lambda pkt,l:l+4),
-                    ShortField("count", 0),
-                    FieldListField("swids",
-                                   [],
-                                   IntField("", 0),
-                                   length_from=lambda pkt:pkt.count*4) ]
-def handle_pkt(pkt):
-    if UDP in pkt and pkt[UDP].dport == 1234:
-        print("got a packet")
-        pkt.show2()
-    #    hexdump(pkt)
-        sys.stdout.flush()
+# create a UDP socket object
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# bind the socket to the specified IP address and port number
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 
-def main():
-    ifaces = [i for i in os.listdir('/sys/class/net/') if 'eth' in i]
-    iface = ifaces[0]
-    print("sniffing on %s" % iface)
-    sys.stdout.flush()
-    sniff(iface = iface,
-          prn = lambda x: handle_pkt(x))
+def parse_phasors(phasor_data, settings={"num_phasors": 1, "pmu_measurement_bytes": 8}):
+    phasor = {
+        "magnitude": struct.unpack('>f', phasor_data[0:4])[0],
+        "angle": math.degrees(struct.unpack('>f', phasor_data[4 : 8])[0]),
+    }
+    return [phasor]
 
-if __name__ == '__main__':
-    main()
+def pmu_packet_parser(data, settings={"pmu_measurement_bytes": 8}):
+    # convert each field to correct data type
+    pmu_packet = {
+        "sync": data[0:2],
+        "frame_size": int.from_bytes(data[2:4], byteorder="big"),
+        "id_code": int.from_bytes(data[4:6], byteorder="big"),
+        "soc": (data[6:10]),
+        "frac_sec": (data[10:14]),
+        "stat": int.from_bytes(data[14:16], byteorder="big"),
+        "phasors": parse_phasors(data[16:16 + 2 * settings["pmu_measurement_bytes"]], {"num_phasors": 1, "pmu_measurement_bytes": settings["pmu_measurement_bytes"]}),
+        "freq": data[16 + 2 * settings["pmu_measurement_bytes"]:18 + 2 * settings["pmu_measurement_bytes"]],
+        "dfreq": data[18 + 2 * settings["pmu_measurement_bytes"]:20 + 2 * settings["pmu_measurement_bytes"]],
+        "analog": data[20 + 2 * settings["pmu_measurement_bytes"]:24 + 2 * settings["pmu_measurement_bytes"]],
+        "digital": data[24 + 2 * settings["pmu_measurement_bytes"]:26 + 2 * settings["pmu_measurement_bytes"]],
+    }
+    return pmu_packet
+
+# wait for incoming PMU packets
+if __name__ == "__main__":
+    counter = 0
+    buffer = []
+    predicted_magnitude = 0
+    predicted_pa = 0
+    while True:
+        data, addr = serverSock.recvfrom(1500)  # receive up to 1500 bytes of data
+        counter += 1
+        # print float value of pmu_packet_parser(data)["frame_size"]
+        pmu_data = pmu_packet_parser(data)
+        print("Magnitude: ", pmu_data["phasors"][0]["magnitude"])
+        print("Angle: ", pmu_data["phasors"][0]["angle"])
+        buffer.append(calculate_complex_voltage(pmu_data["phasors"][0]["magnitude"], pmu_data["phasors"][0]["angle"]))
+        if counter % 3 == 0 and counter != 0:
+            complex_voltage_estimate = jpt_algo(buffer[2], buffer[1], buffer[0])
+            mag, pa = phase_angle_and_magnitude_from_complex_voltage(complex_voltage_estimate)
+            buffer = []
+            print("Next predicted magnitude: ", mag)
+            predicted_magnitude = mag
+            print("Next predicted phase angle: ", pa)
+            predicted_pa = pa
+        if counter % 4 == 0 and counter != 0:
+            print("Approximation error for magnitude: ", calculate_approximation_error(pmu_data["phasors"][0]["magnitude"] , predicted_magnitude))
+            print("Approximation error for angle: ", calculate_approximation_error(pmu_data["phasors"][0]["angle"], predicted_pa))
+        print("\n")
--- exercises/basic/send.py
+++ exercises/basic/send.py
@@ -1,39 +1,88 @@
-#!/usr/bin/env python3
-import random
 import socket
+import datetime
+import math
+import struct
+import pandas as pd
 import sys
-
-from scapy.all import IP, TCP, UDP, Ether, get_if_hwaddr, get_if_list, sendp
-
-
-def get_if():
-    ifs=get_if_list()
-    iface=None # "h1-eth0"
-    for i in get_if_list():
-        if "eth0" in i:
-            iface=i
-            break;
-    if not iface:
-        print("Cannot find eth0 interface")
-        exit(1)
-    return iface
-
-def main():
-
-    """
-    if len(sys.argv)<3:
-        print('pass 2 arguments: <destination> "<message>"')
-        exit(1)
-    """
-    addr = socket.gethostbyname("10.0.2.2")
-    iface = get_if()
-
-    print("sending on interface %s to %s" % (iface, str(addr)))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt /IP(dst=addr) / UDP(dport=1234, sport=random.randint(49152,65535)) / "hello"
-    pkt.show2()
-    sendp(pkt, iface=iface, verbose=False)
+sys.path.append('../')
+from utilities.pmu_csv_parser import parse_csv_data
 
 
-if __name__ == '__main__':
-    main()
+def generate_packet(time, voltage, angle, settings={"pmu_measurement_bytes": 8, "destination_ip": "192.168.0.100", "destination_port": 4712}):
+    # Define the PMU packet as a byte string
+    datetime_str = str(time)[:26]
+
+    try:
+        dt = datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        dt = datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+    # 2 byte
+    sync = b'\xAA\x01'
+
+    # 2 byte, 44 for 32 bit values of PMU, 40 for 16 bit values of PMU
+    # 36 - 8 + 8 * number of PMUs || 36 - 8 + 4 * number PMUs
+    frame_size = b'\x00\x24'
+
+    # 2 byte, 12 for this
+    id_code = b'\x00\x0C'
+
+    # 4 byte
+    soc = int(dt.strftime("%s")).to_bytes(4, 'big')
+
+    # 4 byte
+    frac_sec = dt.microsecond.to_bytes(4, 'big')
+
+    # 2 byte (no errors)
+    stat = b'\x00\x00'
+
+    # 4 or 8 byte x number of phasors (see doc, 8 is for float)
+    voltage_bytes = struct.pack('>f', voltage)
+    angle_bytes = struct.pack('>f', math.radians(angle))
+    phasors = voltage_bytes + angle_bytes
+
+    # 2 byte, assumed 60
+    freq = b'\x09\xC4'
+
+    # 2 byte
+    dfreq = b'\x00\x00'
+
+    # 4 byte
+    analog = b'\x42\xC8\x00\x00'
+
+    # 2 byte
+    digital = b'\x3C\x12'
+
+    # 2 byte
+    chk = b'\xD4\x3F'
+
+    pmu_packet = sync + frame_size + id_code + soc + frac_sec + \
+        stat + phasors + freq + dfreq + analog + digital + chk
+
+    # Set the destination IP address and port number
+    destination_ip = '127.0.0.1'
+    destination_port = 4712
+
+    # Create a UDP socket
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Send the PMU packet to the destination IP address and port number
+    udp_socket.sendto(pmu_packet, (destination_ip, destination_port))
+
+    # Close the UDP socket
+    udp_socket.close()
+
+
+if __name__ == "__main__":
+    pmu_data = parse_csv_data(
+        "./pmu12.csv",
+        "TimeTag",
+        ["Magnitude01", "Magnitude02", "Magnitude03"],
+        ["Angle01", "Angle02", "Angle03"]
+    )
+    for i in range(0, len(pmu_data["times"])):
+        print(i)
+        generate_packet(pmu_data["times"][i], pmu_data["magnitudes"]
+                        [0][i], pmu_data["phase_angles"][0][i])
+
+    # generate_packets()
