25c895f4dd1cc55a25ee6f98e266e2103f854a6a
Jack Norris <jacktimothynorris@gmail.com>
2023-05-20 00:44:36 +0100

    adjusted generated random index to exclude first 3

--- exercises/basic/evaluation/generate_missing_indexes.py
+++ exercises/basic/evaluation/generate_missing_indexes.py
@@ -4,9 +4,13 @@ import json
 import argparse
 
 def generate_n_percent_missing(n_percent, data_size):
-
     sample = []
     while len(sample) < data_size * (n_percent / 100):
+        rand_index = 0
+
+        # make sure we don't get the first 3 indexes since those should be in the switch
+        while rand_index <= 2:
+            rand_index = random.randrange(data_size)
         sample.append(random.randrange(data_size))
         #remove duplicates
         sample = list(set(sample))
@@ -19,7 +23,7 @@ if __name__ == "__main__":
                         description='Sends pmu packets',
                         epilog='Text at the bottom of help')
 
-    parser.add_argument("-m", "--missing", default = 10)
+    parser.add_argument("-m", "--missing_percentage", default = 10)
     parser.add_argument("--data_size", default = 100)
     parser.add_argument("-o", "--output", default = "missing-data.json")
     args = parser.parse_args()
