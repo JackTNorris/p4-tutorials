9215af0dbf12943e7db296ecf8ace3c0327e8af2
Jack Norris <jacktimothynorris@gmail.com>
2023-06-14 13:40:48 +0000

    added in evaluation for trials 1-7

--- exercises/basic/evaluation/5k/generate-missing-jsons.sh
+++ exercises/basic/evaluation/5k/generate-missing-jsons.sh
@@ -1,4 +1,7 @@
-for i in {1..20}
+for j in {3..10}
 do
-    python3 ../generate_missing_indexes.py  -m $i --data_size 5000 -o missing-5k-$i-pct.json
+    for i in {1..20}
+    do
+        python3 ../generate_missing_indexes.py  -m $i --data_size 5000 -o trial-$j/missing-5k-$i-pct.json
+    done
 done
